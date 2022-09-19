1f9bea8c91abfbc98347f614739473051bb19a38
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-19 09:28:27 -0300

    Criação das categorias

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5/109d33821638001d1165b46843429364
@@ -0,0 +1,6 @@
+package br.com.alura.loja.modelo;
+
+public enum Categoria {
+
+	
+}
--- loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -2,5 +2,7 @@ package br.com.alura.loja.modelo;
 
 public enum Categoria {
 
-	
+	CELULARES,
+	INFORMATICA,
+	LIVROS;
 }
