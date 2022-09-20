e6b318b0b19e0a2764a394c0b087a7967fbc5545
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-20 19:21:40 -0300

    Inserção de dados pelo Hibernate

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -13,15 +13,12 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		Categoria celulares = new Categoria("CELULARES");
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 		
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celular);
+//		em.persist(celulares);		
+		celulares.setNome("XPTO");
 		em.getTransaction().commit();
 		em.close();
 	}
