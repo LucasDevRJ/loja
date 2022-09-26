2853ed4323b644d80958607de1f6d55c39b5a361
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-26 20:54:10 -0300

    Criação da classe Pedido com seus atributos

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,68 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private BigDecimal valorTotal;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	public Pedido(BigDecimal valorTotal, LocalDate dataCadastro, Cliente cliente) {
+		super();
+		this.valorTotal = valorTotal;
+		this.dataCadastro = dataCadastro;
+		this.cliente = cliente;
+	}
+
+	public Pedido() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}	
+}
