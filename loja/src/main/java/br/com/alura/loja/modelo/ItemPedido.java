4f78334b070c1ebf951704654babc7d87812d44a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-26 21:11:11 -0300

    Criação dos métodos Getters, Setters e Construtor

--- loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
+++ loja/src/main/java/br/com/alura/loja/modelo/ItemPedido.java
@@ -25,4 +25,54 @@ public class ItemPedido {
 	
 	@ManyToOne
 	private Produto produto;
+
+	public ItemPedido(int quantidade, Pedido pedido, Produto produto) {
+		this.quantidade = quantidade;
+		this.pedido = pedido;
+		this.produto = produto;
+	}
+	
+	public ItemPedido() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(BigDecimal precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
 }
