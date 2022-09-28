04b438f6b4c252392a98b273c9039dc1b0c7ab27
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-28 20:58:55 -0300

    Criação do método para pegar o valor total dos produtos somados

--- loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/PedidoDao.java
@@ -1,5 +1,7 @@
 package br.com.alura.loja.dao;
 
+import java.math.BigDecimal;
+
 import javax.persistence.EntityManager;
 import br.com.alura.loja.modelo.Pedido;
 
@@ -13,5 +15,11 @@ public class PedidoDao {
 	
 	public void cadastrar(Pedido pedido) {
 		this.em.persist(pedido);
+	}
+	
+	public BigDecimal valorTotalVendido() {
+		String jpql = "SELECT SUM(p.valorTotal) FROM Pedido p";
+		return em.createQuery(jpql, BigDecimal.class)
+				.getSingleResult();
 	}
 }
