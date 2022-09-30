8885e6a2dbe3b06c4254f26cdb8600a87e8af110
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-30 08:56:14 -0300

    Criação do método que busca produto por parâmetros

--- loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,9 +1,11 @@
 package br.com.alura.loja.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
@@ -57,5 +59,33 @@ public class ProdutoDao {
 		return em.createQuery(jpql, BigDecimal.class)
 				.setParameter("nome", nome)
 				.getSingleResult();
+	}
+	
+	public List<Produto> buscarPorParametros(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		String jpql = "SELECT p FROM Produto p WHERE 1=1";
+		
+		if (nome != null && !nome.trim().isEmpty()) {
+			jpql = " WHERE p.nome = :nome";
+		}
+		if (preco != null) {
+			jpql = " AND p.preco = :preco";
+		}
+		if (dataCadastro != null) {
+			jpql = " AND p.dataCadastro = :dataCadastro";
+		}
+		
+		TypedQuery<Produto> query = em.createQuery(jpql, Produto.class);
+		
+		if (nome != null && !nome.trim().isEmpty()) {
+			query.setParameter("nome", nome);
+		}
+		if (preco != null) {
+			query.setParameter("preco", preco);
+		}
+		if (dataCadastro != null) {
+			query.setParameter("dataCadastro", dataCadastro);
+		}
+		
+		return query.getResultList();
 	}
 }
