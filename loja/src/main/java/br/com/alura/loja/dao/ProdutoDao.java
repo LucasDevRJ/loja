94f542d90e6ee918519cb72a5f7047b6bf7f62bf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-10 10:26:25 -0300

    construtor do ProdutoDao]

--- loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ loja/src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -1,5 +1,12 @@
 package br.com.alura.loja.dao;
 
+import javax.persistence.EntityManager;
+
 public class ProdutoDao {
 
+	private EntityManager em;
+	
+	public ProdutoDao(EntityManager em) {
+		this.em = em;
+	}
 }
