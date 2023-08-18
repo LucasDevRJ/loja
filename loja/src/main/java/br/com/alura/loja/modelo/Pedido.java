4fcac92706f6c827f952d2ffb3f9085d5d50caec
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-18 20:07:36 -0300

    classe Pedido com seus atributos, método e anotações criada

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,65 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Cliente cliente;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();
+	
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public Pedido() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+}
