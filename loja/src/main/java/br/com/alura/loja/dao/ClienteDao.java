321333d85fa97c70366c11355b73ca6c64b2dc6a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-19 19:04:07 -0300

    criação da classe ClienteDao

---
+++ loja/src/main/java/br/com/alura/loja/dao/ClienteDao.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja.dao;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.modelo.Cliente;
+
+public class ClienteDao {
+
+	private EntityManager em;
+	
+	public ClienteDao(EntityManager em) {
+		this.em = em;
+	}
+	
+	public void cadastrar(Cliente cliente) {
+		this.em.persist(cliente);
+	}
+}
