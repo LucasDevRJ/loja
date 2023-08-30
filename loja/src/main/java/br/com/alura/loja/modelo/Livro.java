288eb66074b5b81730d8d0cbbca9d5487b67969b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-08-30 20:20:36 -0300

    atributos e construtores da classe

--- loja/src/main/java/br/com/alura/loja/modelo/Livro.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Livro.java
@@ -1,5 +1,36 @@
 package br.com.alura.loja.modelo;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "livros")
 public class Livro extends Produto {
 
+	private String autor;
+	private Integer numeroDePaginas;
+	
+	public Livro(String nome, String descricao, BigDecimal preco, Categoria categoria, String autor,
+			Integer numeroDePaginas) {
+		this.autor = autor;
+		this.numeroDePaginas = numeroDePaginas;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+	
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public Integer getNumeroDePaginas() {
+		return numeroDePaginas;
+	}
+	
+	public void setNumeroDePaginas(Integer numeroDePaginas) {
+		this.numeroDePaginas = numeroDePaginas;
+	}
 }
